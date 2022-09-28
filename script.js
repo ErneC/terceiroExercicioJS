ce234b1cb5ed2e202aa9375dac090d636bc4d434
ErneC <mv.ernestina@gmail.com>
2022-09-27 21:53:01 -0300

    terceiro exercicio JS

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+//console.log(document.querySelector("#div1"));
+
+//console.log(document.querySelector(".div2"));
+
+
+//console.log(document.querySelector("ul"));
+
+//console.log(document.querySelector("ul li"));
+
+//console.log(document.querySelectorAll("ul li"));
+
+
+console.log(document.querySelectorAll("ul li")[2]);
+
+
+//Acessa quaisquer tags.
